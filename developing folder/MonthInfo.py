a8f7d3ddd77088e0bb3d382bda3562d5724f3103
SongDaeSun <backgom07@gmail.com>
2019-08-21 15:43:24 +0900

    edit weekday function from MonthInfo

--- developing folder/MonthInfo.py
+++ developing folder/MonthInfo.py
@@ -7,22 +7,21 @@ class Time ():
         self.month = month
         self.day = day
 
+    def weekday(self):
+        return calendar.weekday(self.year, self.month, self.day)
+
 class MonthInfo():
     def __init__ (self,year,month):
         self.primeTime = Time(year, month, 1)
         self.startingDays = list()
         currentDay = copy.deepcopy(self.primeTime)
 
-        while calendar.weekday(currentDay.year, currentDay.month, currentDay.day) != 6:
+        while currentDay.weekday() != 6:
             currentDay.day += 1
         
         while currentDay.day <= calendar.monthrange(year, month)[1]:
-            #print(currentDay.day)
+            print(currentDay.day)
             self.startingDays.append(copy.deepcopy(currentDay))
             currentDay.day+=7
 
-
-#test = MonthInfo(2019, 8)
-
-
-    
+test = MonthInfo(2019, 8)
