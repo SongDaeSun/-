a5d73b3ae5e45d886b2b54ff2038a642053189bd
SongDaeSun <backgom07@gmail.com>
2019-09-29 00:14:41 +0900

    BackUp

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource clickButtonSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPlay()
+    {
+        Sound = clickButtonSound.clip;
+        clickButtonSound.Play();
+    }
+}
